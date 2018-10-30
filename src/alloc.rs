06be16c7722d5284c31a0db1119f0ade9d5eec8f
Colin Valliant <alcarithemad@gmail.com>
2018-10-29 21:37:40 -0700

    Update for alloc changes.

--- src/alloc.rs
+++ src/alloc.rs
@@ -7,20 +7,20 @@ pub struct KernelAllocator;
 
 #[cfg(target_env="kext")]
 unsafe impl alloc::GlobalAlloc for KernelAllocator {
-    unsafe fn alloc(&self, layout: alloc::Layout) -> *mut alloc::Opaque {
+    unsafe fn alloc(&self, layout: alloc::Layout) -> *mut u8 {
         kernel::raw::_MALLOC(
             layout.size(),
             0, // not sure what type is for
             0, // not sure what flags is for
-        ) as *mut alloc::Opaque
+        ) as *mut u8
     }
-    unsafe fn dealloc(&self, ptr: *mut alloc::Opaque, _layout: alloc::Layout) {
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: alloc::Layout) {
         kernel::raw::_FREE(ptr as *mut c_types::c_void, 0)
     }
 }
 
 #[cfg(target_env="kext")]
 #[lang = "oom"]
-extern "C" fn oom(_err: alloc::AllocErr) -> ! {
+extern "C" fn oom(_err: alloc::Layout) -> ! {
     panic!("Out of memory!");
 }
