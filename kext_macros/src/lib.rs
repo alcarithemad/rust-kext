d224b3d1aafac546e4c58c8d7ff0290282fc1162
Colin Valliant <alcarithemad@gmail.com>
2018-10-30 23:50:26 -0700

    `use` cleanup.

--- kext_macros/src/lib.rs
+++ kext_macros/src/lib.rs
@@ -7,16 +7,15 @@ extern crate quote;
 extern crate syn;
 
 use quote::quote;
-use syn::parse_macro_input;
-use syn::LitByteStr;
 use syn::fold::Fold;
+use syn::Expr;
 
 struct BytesFolder();
 
 impl syn::fold::Fold for BytesFolder {
-    fn fold_expr(&mut self, i: syn::Expr) -> syn::Expr {
+    fn fold_expr(&mut self, i: Expr) -> Expr {
         match i {
-            syn::Expr::Lit(syn::ExprLit {attrs: _, lit: syn::Lit::ByteStr(b)}) => {
+            Expr::Lit(syn::ExprLit {attrs: _, lit: syn::Lit::ByteStr(b)}) => {
                 let mut v = b.value().clone();
                 v.resize(64, 0);
                 let v_final = proc_macro2::Literal::byte_string(v.as_slice());
@@ -31,7 +30,7 @@ impl syn::fold::Fold for BytesFolder {
 
 #[proc_macro]
 pub fn bytes_to_u8_64(item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as syn::Expr);
+    let input = syn::parse_macro_input!(item as Expr);
     let mut folder = BytesFolder();
     let output = folder.fold_expr(input);
     TokenStream::from(quote::quote!(#output))
